1866c17ee7ad34aca52fcbf04a9c90d83db8f1d8
atharv-31 <atharvjagdale221@gmail.com>
2025-11-21 16:16:39 +0530

    Changes in Employee Class

--- EmpolyeeProj1/src/com/app/InserData.java
+++ EmpolyeeProj1/src/com/app/InserData.java
@@ -1,8 +1,36 @@
 package com.app;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.entity.Employee;
 public class InserData {
 	public static void main(String[] args) {
 		
+		Employee employee=new Employee();
+		
+		employee.setName("Atharv");
+		employee.setSalary(30000);
+		
+		Configuration config=new Configuration();
+		
+		config.configure("hibernate.cfg.xml");
+		
+		SessionFactory factory = config.buildSessionFactory();
+		
+		Session session =factory.openSession();
+		
+		Transaction tx = session.beginTransaction();
+
+		session.save(employee);
+
+		tx.commit();
+
+		session.close();
+
+		factory.close();
 	}
 
 }
